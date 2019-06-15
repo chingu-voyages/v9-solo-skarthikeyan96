981059454e68f1de3769ed6b5dba933b1d0b34f2
karthikeyan <imkarthikeyans@gmail.com>
2019-06-15 17:41:14 +0530

    Navbar and prod build

---
+++ build/precache-manifest.47b9434e0bc3ea748ca298d86cdabbb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7fdd75f808149d99fffb7beae8f18d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44b50a263fe1591eaab4",
+    "url": "/static/css/2.ab7e5612.chunk.css"
+  },
+  {
+    "revision": "e6a18bbfe7874d753460",
+    "url": "/static/css/main.fb5fa5fb.chunk.css"
+  },
+  {
+    "revision": "44b50a263fe1591eaab4",
+    "url": "/static/js/2.3162f6ee.chunk.js"
+  },
+  {
+    "revision": "e6a18bbfe7874d753460",
+    "url": "/static/js/main.a17983e7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import { Layout, Menu, Row, Col } from "antd";
+import { Icon } from "react-icons-kit";
+import { film } from "react-icons-kit/fa/film";
+import "./styles.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movieDetails: [],
+      currentPage: 1
+    };
+  }
+  componentDidMount() {
+    
+  }
+  render() {
+    const { SubMenu } = Menu;
+    const { Header, Content, Sider } = Layout;
+    return (
+      <div className="App">
+        <Layout>
+          <Header className="header">
+            <Icon
+              size={32}
+              icon={film}
+              style={{ color: "#ffffff", float: "left" }}
+            />
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              style={{ lineHeight: "64px", fontSize: "1.2em" }}
+            >
+              <Menu.Item key="1">React Movie</Menu.Item>
+            </Menu>
+          </Header>
+          <Content className="content">
+            <div className="main">
+              <Row>
+                <Col span={12} />
+              </Row>
+            </div>
+          </Content>
+        </Layout>
+      </div>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
